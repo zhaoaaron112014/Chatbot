81183ba551e9419d449e51f7522873113b918874
zhaoaaron112014 <60793080+zhaoaaron112014@users.noreply.github.com>
2020-02-21 12:25:24 -0800

    Add files via upload

--- CreateAndOpen.java
+++ CreateAndOpen.java
@@ -1,3 +1,20 @@
 public class CreateAndOpen extends MagpieMain{
 
+    public String getCAOGreeting(){
+        return "What would you like to know about Creating and Opening projects?";
+    }
+
+    public String getCAOResponse(String statement){
+        String response = "";
+        if (findKeyword(statement, "create")>= 0 || findKeyword(statement, "new")>= 0){
+            response = "To create a new project, click File > New> Project.\nNext, select Java and any additional libraries you need, before clicking Next.\n" +
+                    "If you have a template to use, select the option, then click Next.\nThen, select the folder you would like to put your project in, before clicking Finish.";
+        }
+        return response;
+
+
+    }
+
+
+
 }
--- FilesAndFolders.java
+++ FilesAndFolders.java
@@ -1,7 +1,9 @@
 public class FilesAndFolders extends MagpieMain{
 
 
-
+    public String getFAFGreeting(){
+        return "What would you like to know about Files and Folders?";
+    }
 
 
 

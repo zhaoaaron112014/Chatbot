b9f220c230444e55ef80650dc391a98bdae92fb9
beatrizmaldo <beeactressmaldo@gmail.com>
2020-03-02 21:19:46 -0800

    Update Shortcuts.java

--- Shortcuts.java
+++ Shortcuts.java
@@ -1,5 +1,28 @@
-public class Shortcuts extends MagpieMain{
+public Shortcuts extends MagpieMain {
+  public String ShortcutsIntro() 
+  {
+    return "What type of shortcut would you like to use?";
+  }
 
+  public String Response(String x)
+  {
+    String response = "";
 
+    if (findKeyword (x, "move") > 0)
+    {
+      response = "if you want to move a line to the end of the code, \t press END. \t if you want to move a line to the top of the code, \t press HOME.";
+    }
 
+    if (findKeyword (x, "delete") > 0)
+    {
+     response = "use the command \tctrl + Y\t to delete the current line you are on";
+    }
+
+    if (findKeyword (x, ("copy" || "duplicate")))
+    {
+      response = "Using \tctrl + D\t allows you to duplicate the chosen line ";
+    }
+      
+      //unfinished for editing and additions if above works
+  }
 }

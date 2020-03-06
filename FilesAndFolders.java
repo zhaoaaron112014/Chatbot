bd6619ce90d23d982203d2f8bc214ce9b1a324e9
gavpineda <61251579+gavpineda@users.noreply.github.com>
2020-03-05 17:18:55 -0800

    Update FilesAndFolders.java

--- FilesAndFolders.java
+++ FilesAndFolders.java
@@ -4,14 +4,19 @@ public class FilesAndFolders extends Helper{
     public String getFAFGreeting(){
         return "What would you like to know about Files and Folders?";
     }
+    
+    public String iAmResponse(String statement){
+      for (int i = 0; i < save.length; i++) {
+            if (statement.contains(save[i])) {
+                returned = "If you would like to know how to save your document, click the Save button, and then choose where you would like to put your file";
+
+                return returned;
+            }
 
 
+    private String[] save = {
+            "save"
+    };
+      }
 
 
-
-
-
-
-
-
-}
